200a3f7a845ca24eb2f78e54a490352bcf86bd9a
Brian Mc George <brian.mcgeorge@hetzner.co.za>
2014-11-20 12:43:42 +0200

    Remove the ruby install

--- sql_install/sqlInstall.sh
+++ sql_install/sqlInstall.sh
@@ -5,6 +5,3 @@ apt-get -y install mysql-server-5.1
 apt-get -y install ps-watcher
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
-/usr/local/rvm/bin/rvm install 1.9.3-p3922
-/usr/local/rvm/bin/rvm use 1.9.3-p392
-
